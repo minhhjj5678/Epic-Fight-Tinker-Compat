4205d0ad372001af38f2fb55baa4f6b84f2af087
minhhjj5678 <minhhjj5678@gmail.com>
2026-05-02 22:10:23 +0000

    fix: remove duplicate modifier registration

--- src/main/java/com/minhhjjj/epicfighttinkercompat/modifiers/EpicFightModifiers.java
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/modifiers/EpicFightModifiers.java
@@ -1,5 +1,6 @@
 package com.minhhjjj.epicfighttinkercompat.modifiers;
 
+import slimeknights.tconstruct.library.modifiers.ModifierId;
 import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegister;
 import slimeknights.tconstruct.library.modifiers.util.StaticModifier;
 import com.minhhjjj.epicfighttinkercompat.EpicFightTinkerCompat;
@@ -9,7 +10,7 @@ public class EpicFightModifiers {
     public static final ModifierDeferredRegister MODIFIERS = ModifierDeferredRegister.create(EpicFightTinkerCompat.MODID);
 
     public static final StaticModifier<EpicFightPartStatModifier> EPIC_PART_STATS_CALCULATOR = MODIFIERS.register("epic_part_stats_calculator", EpicFightPartStatModifier::new);
-    public static final StaticModifier<Modifier> TORMENT_BLADE = MODIFIERS.register("torment_blade", Modifier::new);
-    public static final StaticModifier<Modifier> RUINE_BLADE = MODIFIERS.register("ruine_blade", Modifier::new);
-    public static final StaticModifier<Modifier> AGONY_SPEAR = MODIFIERS.register("agony_spear", Modifier::new);
+    public static final ModifierId TORMENT_BLADE = new ModifierId(EpicFightTinkerCompat.MODID,"torment_blade");
+    public static final ModifierId RUINE_BLADE = new ModifierId(EpicFightTinkerCompat.MODID,"ruine_blade");
+    public static final ModifierId AGONY_SPEAR = new ModifierId(EpicFightTinkerCompat.MODID, "agony_spear");
 }
