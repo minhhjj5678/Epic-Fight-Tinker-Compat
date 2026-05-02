0a7e3baa7a0186799edd12dd97e72b6586f7dc84
minhhjj5678 <minhhjj5678@gmail.com>
2026-05-02 22:06:58 +0000

    feat: require 'blocking' modifier for TConstruct tools to perform guard skills

---
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/mixin/skill/MixinGuardSkill.java
@@ -0,0 +1,40 @@
+package com.minhhjjj.epicfighttinkercompat.mixin.skill;
+
+import com.minhhjjj.epicfighttinkercompat.tool.capabilities.TCWeaponCapability;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import slimeknights.tconstruct.TConstruct;
+import slimeknights.tconstruct.library.modifiers.ModifierId;
+import slimeknights.tconstruct.library.tools.item.ModifiableItem;
+import slimeknights.tconstruct.library.tools.nbt.ToolStack;
+import yesman.epicfight.skill.guard.GuardSkill;
+import yesman.epicfight.world.capabilities.entitypatch.player.PlayerPatch;
+import yesman.epicfight.world.capabilities.item.CapabilityItem;
+
+@Mixin(value = GuardSkill.class, remap = false)
+public class MixinGuardSkill {
+    @Unique
+    private static final ModifierId EPICFIGHTTINKERCOMPAT$BLOCKING_ID = new ModifierId(TConstruct.MOD_ID, "blocking");
+
+    @Inject(method = "isHoldingWeaponAvailable", at = @At("HEAD"), cancellable = true)
+    private void epicfighttinkercompat$requireBlockingModifier(PlayerPatch<?> playerpatch, CapabilityItem itemCapability, GuardSkill.BlockType blockType, CallbackInfoReturnable<Boolean> cir) {
+        if (itemCapability == null || playerpatch == null || playerpatch.getOriginal() == null) {
+            return;
+        }
+
+        ItemStack stack = playerpatch.getOriginal().getMainHandItem();
+
+        if (stack.isEmpty() || !(stack.getItem() instanceof ModifiableItem)) {
+            return;
+        }
+
+        ToolStack toolStack = ToolStack.from(stack);
+        if (toolStack.getModifierLevel(EPICFIGHTTINKERCOMPAT$BLOCKING_ID) < 1) {
+            cir.setReturnValue(false);
+        }
+    }
+}
