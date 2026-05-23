da08e55bb55202c7f95d6c4a81b85aa668fbf723
minhhjj5678 <minhhjj5678@gmail.com>
2026-05-23 16:49:45 +0000

    feat: stop using modifier interaction when performing guard skill

--- src/main/java/com/minhhjjj/epicfighttinkercompat/mixin/tool/MixinModifiableItem.java
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/mixin/tool/MixinModifiableItem.java
@@ -10,8 +10,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import slimeknights.tconstruct.library.tools.item.ModifiableItem;
+import slimeknights.tconstruct.library.tools.nbt.ToolStack;
+import yesman.epicfight.skill.SkillSlots;
+import yesman.epicfight.skill.guard.GuardSkill;
 import yesman.epicfight.world.capabilities.EpicFightCapabilities;
 import yesman.epicfight.world.capabilities.entitypatch.player.PlayerPatch;
+
+import static com.minhhjjj.epicfighttinkercompat.modifiers.EpicFightModifiers.BLOCKING;
 
 @Mixin(value = ModifiableItem.class)
 public class MixinModifiableItem {
@@ -19,7 +24,10 @@ public class MixinModifiableItem {
     private void epicfighttinkercompat$cancelUseInEpicFightMode(Level worldIn, Player playerIn, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
         PlayerPatch<?> playerPatch = EpicFightCapabilities.getPlayerPatch(playerIn);
         if (playerPatch != null && playerPatch.isEpicFightMode()) {
-            cir.setReturnValue(InteractionResultHolder.consume(playerIn.getItemInHand(hand)));
+            ToolStack toolStack = ToolStack.from(playerIn.getItemInHand(hand));
+            if (playerPatch.getSkill(SkillSlots.GUARD).getSkill() instanceof GuardSkill && toolStack.getModifierLevel(BLOCKING) > 0 && !playerIn.isCrouching()) {
+                cir.setReturnValue(InteractionResultHolder.consume(playerIn.getItemInHand(hand)));
+            }
         }
     }
 }
