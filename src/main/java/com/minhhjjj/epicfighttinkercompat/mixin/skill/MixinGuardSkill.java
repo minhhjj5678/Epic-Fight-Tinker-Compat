f10183c005009c526cbcae76dba9cea42ff86ae4
minhhjj5678 <minhhjj5678@gmail.com>
2026-05-23 16:52:46 +0000

    feat: prevent performing guard skill when using interaction modifiers

--- src/main/java/com/minhhjjj/epicfighttinkercompat/mixin/skill/MixinGuardSkill.java
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/mixin/skill/MixinGuardSkill.java
@@ -1,6 +1,5 @@
 package com.minhhjjj.epicfighttinkercompat.mixin.skill;
 
-import com.minhhjjj.epicfighttinkercompat.tool.capabilities.TCWeaponCapability;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -8,7 +7,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import slimeknights.tconstruct.TConstruct;
+import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierId;
+import slimeknights.tconstruct.library.modifiers.hook.interaction.GeneralInteractionModifierHook;
 import slimeknights.tconstruct.library.tools.item.ModifiableItem;
 import slimeknights.tconstruct.library.tools.nbt.ToolStack;
 import yesman.epicfight.skill.guard.GuardSkill;
@@ -25,15 +26,20 @@ public class MixinGuardSkill {
         if (itemCapability == null || playerpatch == null || playerpatch.getOriginal() == null) {
             return;
         }
-
         ItemStack stack = playerpatch.getOriginal().getMainHandItem();
+        ToolStack toolStack = ToolStack.from(stack);
 
         if (stack.isEmpty() || !(stack.getItem() instanceof ModifiableItem)) {
             return;
         }
 
-        ToolStack toolStack = ToolStack.from(stack);
         if (toolStack.getModifierLevel(EPICFIGHTTINKERCOMPAT$BLOCKING_ID) < 1) {
+            cir.setReturnValue(false);
+        }
+
+        if (toolStack.getModifierList().stream().anyMatch(modifierEntry -> {
+            Modifier modifier = modifierEntry.getModifier();
+            return modifier instanceof GeneralInteractionModifierHook;}) && playerpatch.getOriginal().isCrouching()) {
             cir.setReturnValue(false);
         }
     }
