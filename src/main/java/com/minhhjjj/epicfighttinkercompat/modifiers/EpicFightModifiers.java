46265024beda4c964bab5a79ab88790bba326dae
minhhjj5678 <minhhjj5678@gmail.com>
2026-05-23 16:51:54 +0000

    chore: add ModifierId

--- src/main/java/com/minhhjjj/epicfighttinkercompat/modifiers/EpicFightModifiers.java
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/modifiers/EpicFightModifiers.java
@@ -1,5 +1,6 @@
 package com.minhhjjj.epicfighttinkercompat.modifiers;
 
+import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.modifiers.ModifierId;
 import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegister;
 import slimeknights.tconstruct.library.modifiers.util.StaticModifier;
@@ -10,6 +11,11 @@ public class EpicFightModifiers {
     public static final ModifierDeferredRegister MODIFIERS = ModifierDeferredRegister.create(EpicFightTinkerCompat.MODID);
 
     public static final StaticModifier<EpicFightPartStatModifier> EPIC_PART_STATS_CALCULATOR = MODIFIERS.register("epic_part_stats_calculator", EpicFightPartStatModifier::new);
+
+    public static final ModifierId BLOCKING = new ModifierId(TConstruct.MOD_ID, "blocking");
+    public static final ModifierId THROWING = new ModifierId(TConstruct.MOD_ID, "throwing");
+
+    // WOM
     public static final ModifierId TORMENT_BLADE = new ModifierId(EpicFightTinkerCompat.MODID,"torment_blade");
     public static final ModifierId RUINE_BLADE = new ModifierId(EpicFightTinkerCompat.MODID,"ruine_blade");
     public static final ModifierId AGONY_SPEAR = new ModifierId(EpicFightTinkerCompat.MODID, "agony_spear");
