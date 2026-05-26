e3d5e20a6d3d8251b3dd07a3a45d1c22d46afa3f
minhhjj5678 <minhhjj5678@gmail.com>
2026-05-26 15:03:02 +0000

    feat: disable blocking modifier in epic fight mode

---
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/mixin/tool/MixinBlockingModifier.java
@@ -0,0 +1,27 @@
+package com.minhhjjj.epicfighttinkercompat.mixin.tool;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.hook.interaction.InteractionSource;
+import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+import slimeknights.tconstruct.tools.modifiers.ability.interaction.BlockingModifier;
+import yesman.epicfight.world.capabilities.EpicFightCapabilities;
+import yesman.epicfight.world.capabilities.entitypatch.player.PlayerPatch;
+
+@Mixin(value = BlockingModifier.class, remap = false)
+public class MixinBlockingModifier {
+    @Inject(method = "onToolUse", at = @At("HEAD"), cancellable = true)
+    private void epicfighttinkercompat$onToolUse(IToolStackView tool, ModifierEntry modifier, Player player, InteractionHand hand, InteractionSource source, CallbackInfoReturnable<InteractionResult> cir) {
+        PlayerPatch<?> patch = EpicFightCapabilities.getPlayerPatch(player);
+        if (patch != null && patch.isEpicFightMode()) {
+            cir.setReturnValue(InteractionResult.PASS);
+        }
+    }
+}
