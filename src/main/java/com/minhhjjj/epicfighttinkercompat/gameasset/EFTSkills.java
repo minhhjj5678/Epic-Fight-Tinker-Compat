be750a04511be1307464aa0ee193b430c774eb2b
minhhjj5678 <minhhjj5678@gmail.com>
2026-07-25 04:14:00 +0000

    refactor: remove unused skills

--- src/main/java/com/minhhjjj/epicfighttinkercompat/gameasset/EFTSkills.java
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/gameasset/EFTSkills.java
@@ -1,23 +1,16 @@
 package com.minhhjjj.epicfighttinkercompat.gameasset;
 
 import com.minhhjjj.epicfighttinkercompat.EpicFightTinkerCompat;
-import com.minhhjjj.epicfighttinkercompat.skill.ArrowTempestSkill;
-import com.minhhjjj.epicfighttinkercompat.skill.SeekingTempestSkill;
-import com.minhhjjj.epicfighttinkercompat.skill.SwapSkill;
-import com.minhhjjj.epicfighttinkercompat.skill.TinkerRightClickSkill;
+import com.minhhjjj.epicfighttinkercompat.skill.bowinnate.ArrowTempestSkill;
+import com.minhhjjj.epicfighttinkercompat.skill.bowinnate.SeekingTempestSkill;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import yesman.epicfight.api.animation.property.AnimationProperty;
 import yesman.epicfight.api.forgeevent.SkillBuildEvent;
 import yesman.epicfight.api.utils.math.ValueModifier;
 import yesman.epicfight.skill.Skill;
-import yesman.epicfight.skill.SkillBuilder;
-import yesman.epicfight.skill.SkillCategories;
-import yesman.epicfight.skill.weaponinnate.SimpleWeaponInnateSkill;
 import yesman.epicfight.skill.weaponinnate.WeaponInnateSkill;
 import yesman.epicfight.world.damagesource.EpicFightDamageTypeTags;
-import yesman.epicfight.world.damagesource.ExtraDamageInstance;
-import yesman.epicfight.world.damagesource.StunType;
 
 import java.util.Set;
 
@@ -26,16 +19,12 @@ import java.util.Set;
         bus = Mod.EventBusSubscriber.Bus.MOD
 )
 public class EFTSkills {
-    public static Skill TINKER_RIGHT_CLICK_SKILL;
-    public static Skill SWAP_SKILL;
     public static Skill ARROW_TEMPEST_SKILL;
     public static Skill SEEKING_TEMPEST_SKILL;
 
     @SubscribeEvent
     public static void buildSkillEvent(SkillBuildEvent build) {
         SkillBuildEvent.ModRegistryWorker modRegistry = build.createRegistryWorker(EpicFightTinkerCompat.MODID);
-        TINKER_RIGHT_CLICK_SKILL = modRegistry.build("tinker_right_click_skill", TinkerRightClickSkill::new, Skill.createBuilder().setCategory(SkillCategories.GUARD).setResource(Skill.Resource.NONE));
-        SWAP_SKILL = modRegistry.build("swap_skill", SwapSkill::new, Skill.createBuilder().setCategory(SkillCategories.WEAPON_PASSIVE).setResource(Skill.Resource.NONE).setActivateType(Skill.ActivateType.ONE_SHOT));
         WeaponInnateSkill arrowTempestSkill = modRegistry.build("arrow_tempest", ArrowTempestSkill::new, WeaponInnateSkill.createWeaponInnateBuilder().setResource(Skill.Resource.COOLDOWN));
         arrowTempestSkill.newProperty().addProperty(AnimationProperty.AttackPhaseProperty.MAX_STRIKES_MODIFIER, ValueModifier.setter(1.0F))
                 .addProperty(AnimationProperty.AttackPhaseProperty.DAMAGE_MODIFIER, ValueModifier.multiplier(2.5F))
